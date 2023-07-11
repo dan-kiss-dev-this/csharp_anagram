1d39e2dd74ae552eacbea6e81248a08d03adb9a0
Daniel Kiss <flowfast47@gmail.com>
2023-07-11 16:03:03 -0700

    Add a console UI Co-authored-by: Elle Hailu <hailu.elle@gmail.com>

--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -1,12 +1,47 @@
 using System;
 using Anagram.Models;
 
-namespace Anagram{
-   class Program
+namespace Anagram
 {
-    static void Main()
+    class Program
     {
-        // ui logic here
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Anagram Finder");
+            Console.WriteLine("Please enter a main word:");
+            string mainWord = Console.ReadLine();
+            AnagramFinder newAnagramFinder = new AnagramFinder(mainWord);
+            ComparisonWords(newAnagramFinder);
+        }
+
+        //function takes in comparison words
+        static void ComparisonWords(AnagramFinder newAnagramFinder)
+        {
+            Console.WriteLine("Please enter a comparison word: ");
+            string comparisonWord = Console.ReadLine();
+            newAnagramFinder.CompareMaker(comparisonWord);
+            Console.WriteLine("Would you like to add another word? Press 1 for yes, or press any other key no:");
+            string userAnswer = Console.ReadLine();
+            if (userAnswer == "1")
+            {
+                ComparisonWords(newAnagramFinder);
+            }
+            else
+            {
+                Comparison(newAnagramFinder);
+            }
+        }
+
+        static void Comparison(AnagramFinder newAnagramFinder)
+        {
+            newAnagramFinder.SortWordList();
+            Console.WriteLine("Here is the list of anagrams:");
+            foreach (string userWord in newAnagramFinder.finalAnagram)
+            {
+                Console.WriteLine(userWord);
+            }
+            Console.WriteLine("-------");
+            Main();
+        }
     }
-} 
 }
