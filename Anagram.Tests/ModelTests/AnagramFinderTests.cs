bda66467ed1fe128bb70f56539f54370890454b1
Daniel Kiss <flowfast47@gmail.com>
2023-07-11 11:29:03 -0700

    add test for including a compareWords list Co-authored-by: Elle Hailu hailu.elle@gmail.com

--- Anagram.Tests/ModelTests/AnagramFinderTests.cs
+++ Anagram.Tests/ModelTests/AnagramFinderTests.cs
@@ -13,7 +13,7 @@ namespace Anagram.Tests
         [TestMethod]
         public void AnagramConstructor_CreatesAnagramObject_AnagramFinder()
         {
-            string userInput = "computer";
+            string userInput = "noon";
             AnagramFinder newAnagram = new AnagramFinder(userInput);
             Assert.AreEqual(typeof(AnagramFinder), newAnagram.GetType());
         }
@@ -21,10 +21,27 @@ namespace Anagram.Tests
         [TestMethod]
         public void AnagramConstructor_ChecksWord_String()
         {
-            string userInput = "computer";
+            string userInput = "noon";
             AnagramFinder newAnagram = new AnagramFinder(userInput);
             string originalWord = newAnagram.MainWord;
             Assert.AreEqual(userInput, originalWord);
+        }
+
+        [TestMethod]
+        public void CompareMaker_HoldsWordsToCheckForAnagrams_ListOfStrings()
+        {
+            string userInput = "noon";
+            AnagramFinder newAnagram = new AnagramFinder(userInput);
+
+            List<string> sampleComparedWords = new List<string> { "nono", "ooon" };
+            string compareWord1 = "nono";
+            string compareWord2 = "ooon";
+            // we want to add compare words   
+            newAnagram.CompareMaker(compareWord1);
+            newAnagram.CompareMaker(compareWord2);
+            List<string> comparedWords = new List<string> { compareWord1, compareWord2 };
+            List<string> returnedCompareWords = newAnagram.GetCompareWords();
+            CollectionAssert.AreEqual(returnedCompareWords, comparedWords);
         }
     }
 }
--- Anagram/Models/AnagramFinder.cs
+++ Anagram/Models/AnagramFinder.cs
@@ -5,8 +5,19 @@ namespace Anagram.Models;
 public class AnagramFinder
 {
     public string MainWord { get; }
+    private List<string> compareWords = new List<string> { };
     public AnagramFinder(string userInput)
     {
         MainWord = userInput;
+    }
+
+    public void CompareMaker(string newWord)
+    {
+        compareWords.Add(newWord);
+    }
+
+    public List<string> GetCompareWords()
+    {
+        return compareWords;
     }
 }
